9247f9ee0a015ea3d683ea99271fbd0f0f79b326
Shawn Diviney <sdiviney@totalwine.com>
2022-02-23 19:06:24 -0500

    Update sql_questions.sql

--- sql_questions.sql
+++ sql_questions.sql
@@ -1,8 +1,8 @@
 -- 1. Show list of all ProductCategoryNames and the total number of products contained with in each category.
-
 -- Follow up: Show the average UnitCost for each ProductCategoryName/ProductSubcategoryName combination.
 
 -- 2. Show list of all Store Names along with the total number of machines associated with each store. Exclude stores which are closed.
+-- Follow up: Use a window function instead of group by to find the same result (or use group by if window function was used already).
 
 -- 3. How many sales from FactSales have promotions (DimPromotions) which ended before or started after the date of the sale? Exclude sales with the “No Discount” promotion from this count.
 
