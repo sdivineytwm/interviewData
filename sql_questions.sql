5d3303ef8b2ba01e38c156a0b6dceb58c0ffc09a
Shawn Diviney <sdiviney@totalwine.com>
2022-02-28 11:23:47 -0500

    Update sql_questions.sql

--- sql_questions.sql
+++ sql_questions.sql
@@ -1,14 +1,24 @@
 -- 1. Show list of all ProductCategoryNames and the total number of products contained with in each category.
 -- Follow up: Show the average UnitCost for each ProductCategoryName/ProductSubcategoryName combination.
 
+
+
 -- 2. Show list of all Store Names along with the total number of machines associated with each store. Exclude stores which are closed.
 -- Follow up: Use a window function instead of group by to find the same result (or use group by if window function was used already).
 
+
+
 -- 3. How many sales from FactSales have promotions (DimPromotions) which ended before or started after the date of the sale? Exclude sales with the “No Discount” promotion from this count.
+
+
 
 -- 4. Show a list of all customers whose birthday is today.
 
+
+
 -- 5. Which employee of the employees no longer with the company (has EndDate in DimEmployee) had the longest duration of employment (in days)?
+
+
 
 -- ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
